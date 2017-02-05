8460161dd90ce9f92d0191fd4837f720a4634b5a
seradiazpin <seradiazpin@unal.edu.co>
2017-02-04 19:19:12 -0500

    Learning machine vision

---
+++ mv.tutorial.scanner/DocScanner.py
@@ -0,0 +1,8 @@
+from pyimagesearch.transform import four_point_transform
+from pyimagesearch import imutils
+from skimage.filters import threshold_adaptive
+import numpy as np
+import argparse
+import cv2
+
+print cv2.__version__
